212d8e12948f059794c831876ce5f80b5ac08d9d
Godwin <goodgodwin@hotmail.com>
2013-10-28 23:23:57 -0600

    Attempt #4

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,6 +47,11 @@ Starterapp::Application.configure do
 
   config.assets.initialize_on_precompile = false
 
+  config.cache_classes = true
+  config.serve_static_assets = true
+  config.assets.compile = true
+  config.assets.digest = true
+
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
 
