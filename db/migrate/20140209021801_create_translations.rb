1a5af29d14f2c0f099a7dafe1565cbea2fd23d7f
Godwin <goodgodwin@hotmail.com>
2014-03-09 15:47:42 -0600

    The working basics

--- db/migrate/20140209021801_create_translations.rb
+++ db/migrate/20140209021801_create_translations.rb
@@ -1,5 +1,5 @@
 class CreateTranslations < ActiveRecord::Migration
-	def self.up
+	def change
       create_table :translations do |t|
         t.string :locale
         t.string :key
@@ -9,9 +9,5 @@ class CreateTranslations < ActiveRecord::Migration
 
         t.timestamps
       end
-    end
-
-    def self.down
-      drop_table :translations
     end
 end
