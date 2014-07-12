c747c488d0819059fde416cb98bf4037d3a6ebd1
Godwin <goodgodwin@hotmail.com>
2014-07-11 21:48:56 -0600

    Might fix redirect issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,9 +8,9 @@ class ApplicationController < ActionController::Base
 	# For APIs, you may want to use :null_session instead.
 	protect_from_forgery with: :exception
 	
-	if ENV['RAILS_ENV'] || 'production'
-		force_ssl only: :success
-	end
+	#if ENV['RAILS_ENV'] || 'production'
+	#	force_ssl only: :success
+	#end
 
 	before_filter :capture_page_info
 
