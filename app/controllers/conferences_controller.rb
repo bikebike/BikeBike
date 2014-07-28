551f92ba4d189d3c2173d2fd42e9a5e06fe59563
Godwin <goodgodwin@hotmail.com>
2014-07-28 13:00:19 -0600

    wtf?

--- app/controllers/conferences_controller.rb
+++ app/controllers/conferences_controller.rb
@@ -437,9 +437,6 @@ class ConferencesController < ApplicationController
 		end
 		@actions = nil
 		@multipart = false
-		if @register_step == 'register'
-			session.delete(:registration)
-		end
 		case @register_step
 			when  'register', 'organizations', 'new_organization', 'new_workshop', 'volunteer_questions'
 				@actions = :next
