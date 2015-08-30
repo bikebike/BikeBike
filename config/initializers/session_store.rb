111218f9867f9eb21d73eeab57585b6cadf2f24d
Godwin <goodgodwin@hotmail.com>
2015-08-30 10:47:38 -0700

    Sharing session across domains

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-BikeBike::Application.config.session_store :active_record_store
+BikeBike::Application.config.session_store :active_record_store, :domain => :all
