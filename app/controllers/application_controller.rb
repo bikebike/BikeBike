553496d51d84912a4aa43906ca0bcc213d134005
Godwin <goodgodwin@hotmail.com>
2015-09-07 12:46:24 -0700

    Removed forgery protection from email confirmation page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ end
 class ApplicationController < LinguaFrancaApplicationController
 	# Prevent CSRF attacks by raising an exception.
 	# For APIs, you may want to use :null_session instead.
-	protect_from_forgery with: :exception
+	protect_from_forgery with: :exception, :except => [:do_confirm]
 
 	before_filter :capture_page_info
 
