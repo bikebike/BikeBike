80000057a6267989ce942b33e5a0122f4e869f2a
Godwin <goodgodwin@hotmail.com>
2016-12-14 09:09:37 -0800

    Update city_cache.rb

--- app/models/city_cache.rb
+++ app/models/city_cache.rb
@@ -3,11 +3,19 @@ class CityCache < ActiveRecord::Base
 
   belongs_to :city
 
+  # look for a term to see if its already been searched for
   def self.search(str)
-    CityCache.find_by_search(str.downcase)
+    CityCache.find_by_search(normalize_string(str))
   end
 
+  # cache this search term
   def self.cache(str, city_id)
-  	CityCache.create(city_id: city_id, search: str.downcase)
+  	CityCache.create(city_id: city_id, search: normalize_string(str))
   end
+  
+  private
+    def normalize_string(str)
+      # remove accents, unnecessary whitespace, punctuation, and lowcase tje string
+      I18n.transliterate(str).gsub(/[^\w\s]/, '').gsub(/\s\s+/, ' ').strip.downcase
+    end
 end
